cfc939de786e7f1dfbc973f024ae68ee1921aaab
Phillip Wong <deepelegant99@gmail.com>
2021-10-23 19:29:56 -0700

    Added Post, and Fetch between client, and server on the geolocation data

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
-
 const app = express();
+const port= 3000;
 
-app.listen(3000, ()=>console.log("I am listening"))
+app.listen(port, ()=>console.log("Listing at Port "+ port))
 
 app.use(express.static('public'))
+
+// This is to limit the around of incoming data from any client. 
+// I this case, it is limiting the income data to 1mb. 
+app.use(express.json({limit: '1mb'})) 
+
+app.post('/api', (req, res)=>{
+    console.log("I got a request"); 
+   // console.log(req.body)
+    res.send({
+        status: "Success", 
+        lat: req.body.lat, 
+        long: req.body.long,  
+    })
+})
