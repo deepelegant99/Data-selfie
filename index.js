4ecf24b851a79ee3354986e2dae0263a38a83a43
Phillip Wong <deepelegant99@gmail.com>
2021-10-24 00:21:42 -0700

    Added a new field, timestamp, and database using endb(Very similar to Mongodb

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const express = require('express')
+const express = require("express");
+
+const Datastore = require("nedb");
+
 const app = express();
-const port= 3000;
 
-app.listen(port, ()=>console.log("Listing at Port "+ port))
+const database = new Datastore('database.db')
+database.loadDatabase();
 
-app.use(express.static('public'))
 
-// This is to limit the around of incoming data from any client. 
-// I this case, it is limiting the income data to 1mb. 
-app.use(express.json({limit: '1mb'})) 
+const port = 4000;
 
-app.post('/api', (req, res)=>{
-    console.log("I got a request"); 
-   // console.log(req.body)
-    res.send({
-        status: "Success", 
-        lat: req.body.lat, 
-        long: req.body.long,  
-    })
-})
+app.listen(port, () => console.log("Listing at Port " + port));
+
+app.use(express.static("public"));
+
+// This is to limit the around of incoming data from any client.
+// I this case, it is limiting the income data to 1mb.
+app.use(express.json({ limit: "1mb" }));
+
+ 
+
+app.post("/api", (req, res) => {
+  console.log("I got a request");
+
+  let data = {
+    status: "Success",
+    lat: req.body.lat,
+    long: req.body.long,
+    timestamp: Date.now()
+  };
+
+  database.insert(data)
+
+  
+
+  res.send(data);
+});
